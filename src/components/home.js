c1c5d565e93b77f24d79ffb6b4b6f5792dd14dba
github username <github email address>
2019-04-17 15:12:14 -0400

    separate car...

--- src/components/home.js
+++ src/components/home.js
@@ -6,7 +6,9 @@ class Home extends React.Component {
 
   state ={
     start: 0,
-    end: 5
+    end: 5,
+    s:0,
+    e:5
   }
 
 
@@ -29,6 +31,21 @@ class Home extends React.Component {
           }).slice(this.state.start,this.state.end)}
           </div>
           <div className='nxtBtnCont' onClick={this.state.end <= this.props.books.length && this.state.end >= this.props.books.length ? () => this.setState({start: 0, end: 5}) : () => this.setState({start: this.state.start +5,end: this.state.end +5 })}>
+            <div className='nxtBtn'></div>
+          </div>
+        </div>
+
+        <div className='homecar'>
+        <label className='carLabel'>Newly Listed</label>
+          <div className='prevBtnCont' onClick={this.state.s >= 0 && this.state.s <= 5 ? () => this.setState({s: this.props.books.length - 5, e: this.props.books.length }) : () => this.setState({s: this.state.s -5,e: this.state.e -5 })}>
+            <div className='prevBtn'></div>
+          </div>
+          <div className='carcont'>
+          {this.props.books.map(bookObj => {
+            return <CarCard bookShow={this.props.bookShow} key={bookObj.id} book={bookObj}/>
+          }).slice(this.state.s,this.state.e)}
+          </div>
+          <div className='nxtBtnCont' onClick={this.state.e <= this.props.books.length && this.state.e >= this.props.books.length ? () => this.setState({s: 0, e: 5}) : () => this.setState({s: this.state.s +5,e: this.state.e +5 })}>
             <div className='nxtBtn'></div>
           </div>
         </div>
